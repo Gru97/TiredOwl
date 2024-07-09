2f19f930001ed8829c839c19102eefcaaa5677dd
f.shahidani <f.shahidani@asax.ir>
2024-07-09 16:04:53 +0330

    init

---
+++ TiredOwl.Domain.Test/CampaignTests.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace TiredOwl.Domain.Test;
+
+public class CampaignTests
+{
+    [Fact]
+    public void Should_Create_Campaign()
+    {
+        byte[] file = Enumerable.Repeat((byte)0x20, 100).ToArray();
+
+        var campaign = Campaign.Create(Money.Create(100,MoneyUnit.Dollar),
+             new CampaignContent("text goes here"), 
+            CostOption.PerClick, 
+            new LimitlessCampaignStrategy()).WithTopic(Topic.Shopping).WithTopic(Topic.Cloths);
+        
+        campaign.UploadImage(file);
+
+        campaign.Topics.ShouldContain(Topic.Shopping);
+        campaign.CostOption.ShouldBe(CostOption.PerClick);
+        campaign.CampaignStrategy.ShouldBeOfType(typeof(LimitlessCampaignStrategy));
+        //campaign.Content.ShouldBe(new CampaignContent("text goes here"));  //TODO?
+        campaign.Budget.ShouldBe(Money.Create(100, MoneyUnit.Dollar));
+    }
+
+}
+
---
+++ TiredOwl.Domain/Abstract.cs
@@ -0,0 +1,62 @@
+﻿namespace TiredOwl.Domain;
+
+
+public class EnumerableValueObject : ValueObject
+{
+    protected EnumerableValueObject(int value, string name)
+    {
+        Value = value;
+        Name = name;
+    }
+
+    public int Value { get; }
+    public string Name { get; }
+
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+public abstract class ValueObject
+{
+    protected static bool EqualOperator(ValueObject left, ValueObject right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+        {
+            return false;
+        }
+        return ReferenceEquals(left, right) || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+    // Other utility methods
+}
+public class Entity<T>
+{
+    public T Id { get; set; }
+}
